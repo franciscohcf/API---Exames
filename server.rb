469a39fc5dac539f1b84e4987fb3219814a572a2
“franciscohcf” <franciscocavalcantefilho@gmail.com>
2022-04-21 17:58:12 -0300

    fix: server.rb output

--- server.rb
+++ server.rb
@@ -18,5 +18,28 @@ ActiveRecord::Base.establish_connection(
 get '/tests/:token' do
   result = TestResults.where(result_token: params[:token])
 
+  result = result.each_with_object({}) do |result_test, acc|
+    acc['token'] = result_test['result_token']
+    acc['date'] = result_test['result_date']
+    acc['cpf'] = result_test['cpf']
+    acc['name'] = result_test['name']
+    acc['birthday'] = result_test['birthday']
+
+    acc['doctor'] = {
+      'name' => result_test['doctor_name'],
+      'crm' => result_test['doctor_crm'],
+      'crm_state' => result_test['doctor_crm_state']
+    }
+
+    acc['tests'] ||= []
+
+    acc['tests'] << {
+      'type' => result_test['test_type'],
+      'limits' => result_test['test_limits'],
+      'result' => result_test['result']
+    }
+  end
+
+  
   result.to_json
 end
